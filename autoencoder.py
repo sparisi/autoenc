a34496a0d207394fa666b28cf08ecdf0318039ab
parisi <parisi@ias.informatik.tu-darmstadt.de>
2019-02-25 12:25:18 +0100

    first commit

---
+++ autoencoder.py
@@ -0,0 +1,156 @@
+'''
+A collection of some autoencoders presented in
+http://blog.keras.io/building-autoencoders-in-keras.html
+
+* This implementation is supposed to be used with normalized images (the input are pixels whose value is divided by 255).
+* For this reason, the output activation is a sigmoid.
+* All hidden layers have a relu activation (feel free to change it).
+* Autoencoder is a simple (deep) AE whose input is a 1D vector (flattened images).
+* ConvolutionalAutoencoder1D applies 1D convolution to 1D vector.
+* ConvolutionalAutoencoder2D applies 2D convolution to multidimensional vectors (the input can be a gray image, an RGB image, or a series of images).
+* For convolutional AEe, the input has always to be reshaped to have 3 dims.
+* Beside the default "predict" function, all AEe can 1) return the low-dimensional (encoded) image given the original one, 2) return the reconstructed (decoded) image given the encoded one.
+'''
+
+from keras.models import Model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv1D, MaxPooling1D, UpSampling1D, Conv3D, MaxPooling3D, UpSampling3D
+from keras.callbacks import EarlyStopping
+
+
+class AutoencoderBase(object):
+	def train(self, x_train, x_test, epochs, batch_size, stop_early=True):
+		callbacks = []
+		if stop_early:
+			callbacks.append(EarlyStopping(monitor='val_loss',
+                                patience=10,
+                                verbose=1,
+                                mode='auto',
+                                restore_best_weights=True))
+
+		self.autoencoder.fit(x_train, x_train,
+				epochs=epochs,
+				batch_size=batch_size,
+				shuffle=True,
+				validation_data=(x_test, x_test),
+				callbacks=callbacks)
+
+	def predict(self, x):
+		return self.autoencoder.predict(x)
+
+	def encode(self, x):
+		return self.encoder.predict(x)
+
+	def decode(self, x):
+		return self.decoder.predict(x)
+
+	def summary(self):
+		self.autoencoder.summary()
+
+
+class Autoencoder(AutoencoderBase):
+	def __init__(self, dims):
+		dim_in = dims[0] # original image size
+		dim_out = dims[-1] # encoded image size
+		dims_encoder = dims[1:] # hidden layers size
+		dims_decoding = dims[:-1]
+		dims_decoding.reverse()
+
+		input_img = Input(shape=(dim_in,), name='EncoderIn')
+		decoder_input = Input(shape=(dim_out,), name='DecoderIn')
+
+		# construct encoder layers
+		encoded = input_img
+		for i, dim in enumerate(dims_encoder):
+			encoded = Dense(dim, activation='relu', name='Encoder{0}'.format(i))(encoded)
+
+		# construct decoder layers (decoded is connected to encoded, whereas encoded is not connected to decoded)
+		decoded = encoded
+		decoder = decoder_input
+		for i, dim in enumerate(dims_decoding):
+			activation = 'relu'
+			if i == len(dims_decoding) - 1:
+				activation = 'sigmoid'
+			layer = Dense(dim, activation=activation, name='Decoder{0}'.format(i))
+			decoded = layer(decoded)
+			decoder = layer(decoder)
+
+		self.autoencoder = Model(input_img, decoded)
+		self.encoder = Model(input_img, encoded)
+		self.decoder = Model(decoder_input, decoder)
+		self.autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+class ConvolutionalAutoencoder1D(AutoencoderBase):
+	def __init__(self, dims, dim_in):
+		# dims is a list of tuples (channel size, filter size)
+		# eg (128, 9) corresponds to a layer with 128 filters of size 9
+		input_img = Input(shape=(dim_in, 1), name='EncoderIn')
+
+        # each layer halves the size of the image
+		decoder_input = Input(shape=(int(dim_in/2**len(dims)), dims[-1][0]), name='DecoderIn')
+
+		# construct encoder layers
+		encoded = input_img
+		for i, (filters, size) in enumerate(dims):
+			encoded = Conv1D(filters, size, activation='relu', padding='same', name='Conv{0}'.format(i))(encoded)
+			encoded = MaxPooling1D(padding='same', name='MaxPool{0}'.format(i))(encoded)
+
+		# construct decoder layers (decoded is connected to encoded, whereas encoded is not connected to decoded)
+		decoded = encoded
+		decoder = decoder_input
+		for i, (filters, size) in enumerate(reversed(dims)):
+			layer = Conv1D(filters, size, activation='relu', padding='same', name='Deconv{0}'.format(i))
+			decoded = layer(decoded)
+			decoder = layer(decoder)
+			layer = UpSampling1D(name='UpSampling{0}'.format(i))
+			decoded = layer(decoded)
+			decoder = layer(decoder)
+
+		# reduce from X filters to 1 in the output layer
+		layer = Conv1D(1, dims[0][1], activation='sigmoid', padding='same', name='DecoderOut')
+		decoded = layer(decoded)
+		decoder = layer(decoder)
+
+		self.autoencoder = Model(input_img, decoded)
+		self.encoder = Model(input_img, encoded)
+		self.decoder = Model(decoder_input, decoder)
+		self.autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+class ConvolutionalAutoencoder2D(AutoencoderBase):
+	def __init__(self, dims, h, w, c):
+		# dims is a list of tuples (channel size, filter size 1, filter size 2)
+		# eg (128, 3, 3) corresponds to a layer with 128 filters of size 3x3
+
+        # c is the number of channels (3 for rgb images, more if the input is a sequence of images)
+		input_img = Input(shape=(h, w, c), name='EncoderIn')
+
+        # each layer halves the size of the image
+		decoder_input = Input(shape=(int(h/2**len(dims)), int(w/2**len(dims)), dims[-1][0]), name='DecoderIn')
+
+		# construct encoder layers
+		encoded = input_img
+		for i, (filters, rows, cols) in enumerate(dims):
+			encoded = Conv2D(filters, (rows, cols), activation='relu', padding='same', name='Conv{0}'.format(i))(encoded)
+			encoded = MaxPooling2D(padding='same', name='MaxPool{0}'.format(i))(encoded)
+
+		# construct decoder layers (decoded is connected to encoded, whereas encoded is not connected to decoded)
+		decoded = encoded
+		decoder = decoder_input
+		for i, (filters, rows, cols) in enumerate(reversed(dims)):
+			layer = Conv2D(filters, (rows, cols), activation='relu', padding='same', name='Deconv{0}'.format(i))
+			decoded = layer(decoded)
+			decoder = layer(decoder)
+			layer = UpSampling2D(name='UpSampling{0}'.format(i))
+			decoded = layer(decoded)
+			decoder = layer(decoder)
+
+		# reduce from X filters to 1 in the output layer
+		layer = Conv2D(c, (dims[0][1], dims[0][2]), activation='sigmoid', padding='same', name='DecoderOut')
+		decoded = layer(decoded)
+		decoder = layer(decoder)
+
+		self.autoencoder = Model(input_img, decoded)
+		self.encoder = Model(input_img, encoded)
+		self.decoder = Model(decoder_input, decoder)
+		self.autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
---
+++ test.py
@@ -0,0 +1,95 @@
+import os.path
+import sys
+
+import numpy as np
+import scipy.io as sio
+
+import matplotlib.pyplot as plt
+
+from autoencoder import *
+
+DATA_FOLDER = './'
+DATA_FILE = 'ae_test.mat'
+'''
+I, IG, h, w are variables with images (flattened), gray images, height and widht (484x644).
+In I2, IG2, h2, w2, images have been scaled and cropped to (64x64).
+'''
+
+if __name__ == '__main__':
+
+    mode = 'gray'
+    mode = 'rgb'
+
+    data = sio.loadmat(os.path.join(DATA_FOLDER, DATA_FILE))
+    h = np.asscalar(data['h2']) # height
+    w = np.asscalar(data['w2']) # width
+    X = data['I2'] if mode == 'rgb' else data['IG2']
+    c = 3 if mode == 'rgb' else 1
+    X = X / 255. # normalize data
+    n_samples, data_dim = X.shape
+    train_percentage = 0.90 # remainder is used for testing
+
+    (x_train, x_test) = np.split(X, [int(n_samples * train_percentage)])
+    x_train = (x_train.astype('float32'))
+    x_test = (x_test.astype('float32'))
+
+    # learning hyperparameters
+    autoenc_type = 'cae2'
+    epochs = 100
+    batch_size = 32
+
+    # create autoencoder
+    if autoenc_type == 'ae':
+        autoenc = Autoencoder([data_dim, 16, 32])
+    elif autoenc_type == 'cae1':
+        x_train = np.atleast_3d(x_train)
+        x_test = np.atleast_3d(x_test)
+        autoenc = ConvolutionalAutoencoder1D([(16, 2), (32, 1)], h*w)
+    elif autoenc_type == 'cae2':
+        x_train = np.reshape(x_train, (len(x_train), h, w, c), order="F")
+        x_test = np.reshape(x_test, (len(x_test), h, w, c), order="F")
+        autoenc = ConvolutionalAutoencoder2D([(16, 4, 4), (32, 4, 4)], h, w, c)
+    else:
+        raise Exception('Unknown autoencoder type: ' + str(autoenc_type))
+
+    # train
+    autoenc.summary()
+    autoenc.train(x_train, x_test, epochs, batch_size)
+
+    # test
+    # decoded_imgs = autoenc.predict(x_test)
+    encoded_imgs = autoenc.encode(x_test)
+    decoded_imgs = autoenc.decode(encoded_imgs)
+
+    # display original and decoded images
+    n_display = 5
+    fig = plt.figure()
+    im_dims = np.array([h, w]) if mode == 'gray' else np.array([h, w, 3])
+    for i in range(1, n_display+1):
+        ax = plt.subplot(2, n_display, i)
+        plt.imshow(x_test[i].reshape(im_dims, order="F")) # show original
+        if mode == 'gray':
+            plt.gray()
+        plt.axis('off')
+
+        ax = plt.subplot(2, n_display, i + n_display)
+        plt.imshow(decoded_imgs[i].reshape(im_dims, order="F")) # show decoded
+        if mode == 'gray':
+            plt.gray()
+        plt.axis('off')
+
+    plt.show()
+
+    # display final convolution
+    fig = plt.figure()
+    n_channels = 16
+    for i in range(0, n_channels):
+        ax = plt.subplot(1, n_channels, i+1)
+        plt.imshow(encoded_imgs[0][:,:,i]) # show channel
+        if mode == 'gray':
+            plt.gray()
+        plt.axis('off')
+
+    plt.show()
+
+    input("Press Enter to continue...")
